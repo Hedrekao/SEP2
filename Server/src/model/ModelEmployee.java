7b08b1c04d2cc74750d38c45655ce3a4ce4d9d05
Tomek Słoma <82450784+Hedrekao@users.noreply.github.com>
2022-05-06 16:03:25 +0200

    State of app after Sprint2 - day 2
    Login feature implemented; architecture prepared for adding new items/products

--- Server/src/model/ModelEmployee.java
+++ Server/src/model/ModelEmployee.java
@@ -6,4 +6,5 @@ public interface ModelEmployee
 {
   void addUser(String username, String password);
   User getUser(String username, String password);
+  void addItem(String productName, int productID, double price, Date expirationDate, int quantity, ArrayList<Category> categories);
 }
